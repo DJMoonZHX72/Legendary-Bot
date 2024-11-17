c2c4c892f3f6febfdafdff9c5afd5473800512dc
DJMoonZHX72 <raffasyawira1@gmail.com>
2024-11-18 06:08:24 +0700

    Update main.js

--- main.js
+++ main.js
@@ -85,15 +85,11 @@ function getBotResponse(message) {
     } else if (message.includes('redeem code')) {
         return 'Kode Redeem Hari Ini Adalah Legendary72';
     } else if (message.includes('menu')) {
-        return 'Command: redeem code, rank, player info (nama player), weapon list (common/uncommon/rare/legendary/mythic), rules, admin slot, info server, info bot, changelog, support, quiz, calc, achievement, ganti nama';
+        return 'Command: redeem code, rank, weapon list (common/uncommon/rare/legendary/mythic), rules, admin slot, info server, info bot, changelog, support, quiz, calc, achievement, ganti nama';
     } else if (message.includes('achievement')) {
         return displayAchievements();
     } else if (message.includes('rank')) {
         return 'VIP: Harga, MVP: Harga, LEGEND: Harga, MYTHIC: Harga, LUXURY: Harga, HYPER: Harga';
-    } else if (message.includes('player info djmoonzhx72')) {
-        return 'Name: DJMoonZHX72 Rank: CO-OWNER';
-    } else if (message.includes('player info rizkiwibu9696')) {
-        return 'Name: Rizkiwibu9696, Rank: OWNER';
     } else if (message.includes('weapon list common')) {
         return 'Common: Wooden Sword, Golden Sword, Stone Sword, Iron Sword, Shuriken, Basic Hammer, Circus Cluster, Gilded Scythe, Sonic Blade, Spell Twister.';
     } else if (message.includes('weapon list uncommon')) {
@@ -108,8 +104,6 @@ function getBotResponse(message) {
         return 'Rules: No Spam, No Promosi Apapun, No X-ray, No Cheat, No Homo, No Glitch';
     } else if (message.includes('weapon list')) {
         return 'Gunakan weapon list (common/uncommon/rare/legendary/mythic)!';
-    } else if (message.includes('player info')) {
-        return 'Gunakan player info (nama player)!';
     } else if (message.includes('admin slot')) {
         return 'Slot Admin Sisa 10';
     } else if (message.includes('info server')) {
@@ -120,6 +114,8 @@ function getBotResponse(message) {
         return '1.0.0: created bot, 1.1.0: added player info, menu, & rank, 1.2.0: added weapon list, rules, admin slot, info server, & info bot, 1.2.1: added changelog, & support, 1.4.0: added calculator, 1.5.0: added achievement, 1.5.1: updated achievement & quiz, 1.6.0: added name, 1.6.1: bugfix';
     } else if (message.includes('support')) {
         return 'DJMoonZHX72: https://youtube.com/@DJMoonZHX72  https://www.instagram.com/djmoonzhx72/profilecard/?igsh=MXhhczVneWtld3RpdQ==  https://whatsapp.com/channel/0029VarfkCz9mrGkIcsHrW1D https://github.com/DJMoonZHX72 Rizkiwibu9696: https://whatsapp.com/channel/0029Var7OtgGzzKU3Qeq5s09 https://www.instagram.com/ikikidal_03/profilecard/?igsh=dnVnMW5zOXo3dTFo , Legendary Craft: https://whatsapp.com/channel/0029VakZDNU9Gv7TRP0TH53K';
+    } else if (message.includes('⎙')) {
+        return 'https://github.com/DJMoonZHX72/Legendary-Bot/edit/main/main.js';
     } else {
         return 'Maaf, saya tidak mengerti. Ketik "menu" untuk melihat list perintah';
     }
